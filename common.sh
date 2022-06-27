db7c76755fc6a54a94c178c3e1d85dff5356e70d
Kohei Okazaki <okazaki@net.info.hiroshima-cu.ac.jp>
2022-06-27 10:18:14 +0900

    change

--- common.sh
+++ common.sh
@@ -11,3 +11,6 @@ sudo apt-get install -y build-essential
 sudo apt-get install -y libssl-dev
 sudo apt-get install -y git
 sudo apt-get install -y expect
+sudo apt-get install -y python3-pip
+sudo apt-get install -y cmake
+pip3 install setuptools click numpy
--- install_cefore.sh
+++ install_cefore.sh
@@ -14,3 +14,7 @@ cd cefore/ || exit; ./configure --enable-csmgr --enable-cache; cd - || exit
 cd cefore/ || exit; make; cd - || exit
 cd cefore/ || exit; sudo make install; cd - || exit
 cd cefore/ || exit; sudo ldconfig; cd - || exit
+
+git clone https://github.com/cefore/cefpyco.git
+cd cefpyco/ || exit; cmake .; cd - || exit;
+cd cefpyco/ || exit; sudo make install; cd - || exit
